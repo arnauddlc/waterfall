1e7358f8012f23cf5ee4b3853ff39090557ad984
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-03-01 15:28:36 +0800

    add first and last names config for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :charts, dependent: :destroy
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def user_initials
+  	(self.first_name[0] + self.last_name[0]).upcase
+  end
 end
