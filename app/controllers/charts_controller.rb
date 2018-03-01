a049fb22c9d2e467df9aa261ff1704b87850b2d9
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-03-01 11:09:57 +0800

    add routes and controller init

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,10 @@
+class ChartsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'charts/index'
+
+  get 'charts/new'
+
+  get 'charts/edit'
+
   devise_for :users
-  root to: 'pages#home'
+  root to: 'charts#index'
+
+  resources :charts, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
