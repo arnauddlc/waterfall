c8f2f957b8c0a7327d5db4bbd531e59b723a531f
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-03-01 14:53:12 +0800

    add export chart button in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :charts, dependent: :destroy
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
