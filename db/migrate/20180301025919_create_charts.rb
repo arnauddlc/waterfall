b2a05aed09e7959baba94f751fbd147971cb5422
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-03-03 17:49:40 +0800

    trying updating chart with chart design data

--- db/migrate/20180301025919_create_charts.rb
+++ db/migrate/20180301025919_create_charts.rb
@@ -5,7 +5,7 @@ class CreateCharts < ActiveRecord::Migration[5.1]
       t.string :subtitle, default: "chart subtitle"
       t.text :notes
       t.integer :font_size, default: 12
-      t.string :color, default: "$green"
+      t.string :color, default: "$green: #3EC28F"
       t.string :chart_image, default: "http://res.cloudinary.com/arnauddlc/image/upload/v1519980141/waterfall.png"
       t.references :user, foreign_key: true
 
