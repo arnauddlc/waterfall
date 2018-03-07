cb27c6249f92029416f401a80947ac295f63e7e2
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-03-07 20:22:08 +0800

    debug v1

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -14,6 +14,7 @@ class DatasetsController < ApplicationController
   end
 
   def update
+    @chart = @dataset.chart
     @chart.chart_type == "waterfall" ? update_waterfall : update_non_waterfall    
   end
 
